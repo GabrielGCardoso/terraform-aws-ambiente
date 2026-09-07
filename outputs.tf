66ae990e76e03649db2359ab53ef1ebf1af1d8f8
Gabriel Cardoso <gabrielguimac@gmail.com>
2026-09-07 17:52:39 -0300

    refactor: comment out instance_ip output block

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "instance_ip" {
-  description = "IP privado da maquina virtual"
-  value       = aws_instance.this.private_ip
-}
+# output "instance_ip" {
+#   description = "IP privado da maquina virtual"
+#   value       = aws_instance.this.private_ip
+# }
