2ea01268ce1321d5045b8c7d9bc3727bbd533a12
Gabriel Cardoso <gabrielguimac@gmail.com>
2026-09-07 22:31:37 -0300

    fix: instance type

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "this" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = each.value["instance_type"]
+  instance_type = var.instance_type
 
   tags = {
     Name = "name"
